c107d41c19982e3e750f108248a3733c12dd211c
dekrt <dekrt@qq.com>
2023-04-23 20:21:43 +0800

    docs: update InterfaceDesign document

--- code/node_modules/@vant/weapp/dist/circle/canvas.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="miniprogram-api-typings" />
-type CanvasContext = WechatMiniprogram.CanvasContext;
-export declare function adaptor(ctx: CanvasContext & Record<string, unknown>): CanvasContext;
-export {};
