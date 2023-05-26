ee7ec15a6e091eae75aebd5af9ea503073cd2537
dekrt <dekrt@qq.com>
2023-05-26 14:56:53 +0800

    feat: try to build the translation history interface

---
+++ code/pages/history_test/history_test.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    list_BiYy5U1W: [null, null],
+    list_nnpIZ1wY: [null, null],
+    list_xeS18m5q: [null, null],
+    list_Ml983udZ: [null, null],
+    list_WbpwHwPk: [null, null, null, null],
+  },
+
+  onShareAppMessage() {
+    return {};
+  },
+});
