3bc6b3decbdba2294e63e43f9741dcb0fe47ad80
chendeting88 <2697156182@qq.com>
2023-05-26 17:58:14 +0800

    docs:update voice translation and choose language

--- code/pages/Choose Language/Choose Language.js
+++ code/pages/Choose Language/Choose Language.js
@@ -1,66 +1,58 @@
-// pages/Choose Language/Choose Language.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    list_aPbtoDn5: [
+      {
+        "image":"../../image/中文.png",
+        "text": "中文"
+      },
+      {
+        "image":"../../image/英语.png",
+        "text": "英语"
+      },
+      {
+        "image":"../../image/德语.png",
+        "text": "德语"
+      },
+      {
+        "image":"../../image/韩语.png",
+        "text": "韩语"
+      }
+    ],
+    selectedIndex1: null,
+    selectedIndex2: null,
+    isClicked1: false,
+    isClicked2: false,
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage() {
+    return {};
+  },
+  handleTap1: function() {
+    this.setData({
+      isClicked1: false,
+      isClicked2: false,
+    });
+  },
+  handleTap2: function() {
+    this.setData({
+      isClicked2: true,
+      isClicked1: true,
+    });
+  },
+  handleTap: function(e) {
+    var index = e.currentTarget.dataset.index;
+    console.log("Clicked item index: ", index);
+    if (!this.data.isClicked1) {
+      this.setData({
+        selectedIndex1: index
+      });
+    }else{
+      this.setData({
+        selectedIndex2: index
+      });
+    }
 
-  }
-})
+    console.log("Selected item: ", this.data.selectedIndex1,this.data.selectedIndex2);
+  },
+  
+});
